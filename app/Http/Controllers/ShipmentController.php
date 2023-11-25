dfcad5b097fc333e853f979d97aea7334ff85482
James Allen Buan <johnmichaelbuan21@gmail.com>
2023-11-26 01:26:39 +0800

    added

--- app/Http/Controllers/ShipmentController.php
+++ app/Http/Controllers/ShipmentController.php
@@ -29,7 +29,13 @@ class ShipmentController extends Controller
     }
 
     public function myPurchase() {
-        $shipments = Shipment::with('product.user', 'product.productImages', 'user', 'productVariation')->where('status','!=' , 4)->where('user_id', auth()->user()->id)->get();
+        $shipments = Shipment::with('product.user', 'product.productImages', 'user', 'productVariation')
+            ->where(function ($query) {
+                $query->where('status', '!=', 3)
+                    ->orWhereNull('status');
+            })
+            ->where('user_id', auth()->user()->id)
+            ->get();
 
         return view('buyer.myPurchase.index', compact('shipments'));
     }
